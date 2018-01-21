5932b223da40cbfcd7266307edb9f0853526e052
Stefan Kowal <kowalmax.s@gmail.com>
2018-01-20 19:27:22 -0700

    adding public and changing go server

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 	"os"
 
-	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
 )
 
@@ -15,15 +15,20 @@ func main() {
 	if port == "" {
 		log.Fatal("$PORT must be set")
 	}
+	directory := flag.String("d", "public", "usage for dir?")
+	flag.Parse()
+	http.Handle("/", http.FileServer(http.Dir(*directory)))
 
-	router := gin.New()
-	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
-	})
+	// router := gin.New()
+	// router.Use(gin.Logger())
+	// router.LoadHTMLGlob("templates/*.tmpl.html")
+	// router.Static("/public", "public")
 
-	router.Run(":" + port)
+	// router.GET("/", func(c *gin.Context) {
+	// 	c.HTML(http.StatusOK, "public/index.html", nil)
+	// })
+
+	// router.Run(":" + port)
 }
